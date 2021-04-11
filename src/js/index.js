032776e983c0dd8499ec031b761d4bc6c3cc4b3e
skalutki <skalutki@gmail.com>
2021-04-11 10:02:00 +0100

    local storage save and load how many glasses you drank

--- src/js/index.js
+++ src/js/index.js
@@ -10,45 +10,48 @@ const counter= document.querySelector('.counter--js');
 const buttonAdd= document.querySelector('.hydrapp__add--js');
 const buttonRemove= document.querySelector('.hydrapp__remove--js');
 
+//klucz jako data 
+const key = new Date() .toISOString() .slice(0, 10);
+
+
 //sprawdzamy czy nie ma juz czegos zapisanego w local storage
 
-const entry = localStorage.getItem('date');
+const entry = localStorage.getItem(key);
 
 let result = '';
 
 if(entry){
       // zmieniamy string z iloscia szklanek na wartosc number
   result = JSON.parse(entry);
-  counter.innerHTML=localStorage.getItem('date')
+  counter.innerHTML=localStorage.getItem(key)
 }else{
     // Jeśli nie ma entry to zapisz jako 0
     result=0;
-    localStorage.setItem("date", result);
+    localStorage.setItem(key, result);
     
 }
 
 
-
 buttonAdd.addEventListener('click', () => {
-    console.log('dodaj szklanke');
+  //  console.log('dodaj szklanke');
     result=result+1;
-    console.log(result);
-    localStorage.setItem("date", result);
-    counter.innerHTML=localStorage.getItem('date');
+  //  console.log(result);
+    localStorage.setItem(key, result);
+    counter.innerHTML=localStorage.getItem(key);
 });
 
 buttonRemove.addEventListener('click', () => {
-    console.log('usuń szklanke');
+   // console.log('usuń szklanke');
     
 //jesli szklanka bedzie wieksza od 0 to odejmi jesli nie to wyswietl komunikat
 
     if(result>0){
     result=result-1;
-    console.log(result);
-    localStorage.setItem("date", result);    
-    counter.innerHTML=localStorage.getItem('date');
+    //console.log(result);
+    localStorage.setItem(key, result);    
+    counter.innerHTML=localStorage.getItem(key);
     }else{
-        console.log('Nie możesz usunąć szklanki. Jest już 0');
+        alert('Nie możesz usunąć szklanki. Jest już 0');
     }
 });
 
