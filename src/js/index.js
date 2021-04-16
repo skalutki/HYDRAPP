580201e463df6907d8e77174d1f718f88c484948
skalutki <skalutki@gmail.com>
2021-04-16 08:46:17 +0100

    refactoring html / scss / java script. v. 1.0.1

--- src/js/index.js
+++ src/js/index.js
@@ -1,57 +1,43 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-const counter= document.querySelector('.counter--js');
-const buttonAdd= document.querySelector('.hydrapp__add--js');
-const buttonRemove= document.querySelector('.hydrapp__remove--js');
+const counter = document.querySelector(".counter--js");
+const buttonAdd = document.querySelector(".hydrapp__add--js");
+const buttonRemove = document.querySelector(".hydrapp__remove--js");
+//klucz jako data
+const key = new Date().toISOString().slice(0, 10);
 
-//klucz jako data 
-const key = new Date() .toISOString() .slice(0, 10);
-
+let currentGlassCounter = 0;
 
 //sprawdzamy czy nie ma juz czegos zapisanego w local storage
 
-const entry = localStorage.getItem(key);
+const localStorageValue = localStorage.getItem(key);
 
-let result = '';
-
-if(entry){
-      // zmieniamy string z iloscia szklanek na wartosc number
-  result = JSON.parse(entry);
-  counter.innerHTML=localStorage.getItem(key)
-}else{
-    // Jeśli nie ma entry to zapisz jako 0
-    result=0;
-    localStorage.setItem(key, result);
-    
+if (localStorageValue) {
+  currentGlassCounter = localStorageValue;
+} else {
+  localStorage.setItem(key, 0);
 }
 
+counter.innerHTML = currentGlassCounter;
 
-buttonAdd.addEventListener('click', () => {
-  //  console.log('dodaj szklanke');
-    result=result+1;
-  //  console.log(result);
-    localStorage.setItem(key, result);
-    counter.innerHTML=localStorage.getItem(key);
+buttonAdd.addEventListener("click", () => {
+  currentGlassCounter++;
+  counter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
 });
 
-buttonRemove.addEventListener('click', () => {
-   // console.log('usuń szklanke');
-    
-//jesli szklanka bedzie wieksza od 0 to odejmi jesli nie to wyswietl komunikat
-
-    if(result>0){
-    result=result-1;
-    //console.log(result);
-    localStorage.setItem(key, result);    
-    counter.innerHTML=localStorage.getItem(key);
-    }else{
-        alert('Nie możesz usunąć szklanki. Jest już 0');
-    }
+buttonRemove.addEventListener("click", () => {
+  if (currentGlassCounter > 0) {
+    currentGlassCounter--;
+  } else {
+    alert("Nie możesz usunąć szklanki. Jest już 0");
+  }
+  counter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
 });
-
